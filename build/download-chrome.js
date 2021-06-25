10cdbf121513384b463ccd1eeadd2ee3c0cd443f
Igor Gassmann <igor@igassmann.me>
2021-06-25 14:28:23 -0500

    fix: load correct prebuilt binary deps

---
+++ build/download-chrome.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer')
+const revisions = require('puppeteer/lib/cjs/puppeteer/revisions')
+
+if (!process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD) return
+
+let platform = process.env.npm_config_platform
+if (platform === 'win32') platform = 'win64'
+
+const browserFetcher = puppeteer.createBrowserFetcher({platform})
+
+browserFetcher.download(revisions.PUPPETEER_REVISIONS.chromium)
