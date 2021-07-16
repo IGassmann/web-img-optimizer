38014550b1a7851b5828fa8132e45f34bad1e355
Igor Gassmann <igor@igassmann.me>
2021-07-16 14:55:36 -0500

    test: disable auto coverage output

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
-  collectCoverage: true,
 };
 
 export default config;
