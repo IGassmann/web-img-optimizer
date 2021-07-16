3de99c93e17403398d0c103f8dee9c3e855e80e6
Igor Gassmann <igor@igassmann.me>
2021-07-16 14:55:54 -0500

    test: add test for succesful case

--- test/commands/dimensions.test.ts
+++ test/commands/dimensions.test.ts
@@ -1,5 +1,7 @@
 import { test } from '@oclif/test';
 
 describe('dimensions', () => {
-  test.command(['dimensions']).exit(2).it('exits with status 2 when no argument is passed');
+  test.command(['dimensions', 'https://igassmann.me/']).it('Exits without error when successful');
+
+  test.command(['dimensions']).exit(2).it('Exits with status code 2 when no argument is passed');
 });
